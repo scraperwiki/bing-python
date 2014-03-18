254363fd0e0fe541d6a1f2447ab5e89c65113a3d
Tony Walker <tony.walker@local>
2014-03-18 01:33:04 -0700

    Add documentation to py file as well

--- bing.py
+++ bing.py
@@ -1,3 +1,22 @@
+"""
+Bing Search API
+---------------
+
+Usage:
+
+```python
+
+import bing
+
+api = bing.API('your-key')
+
+web_results = api.query('hello world')
+news_results = api.query('hello world', srctype='News')
+
+```
+------------------------------------------------------------
+"""
+
 import json
 import base64
 import urllib
